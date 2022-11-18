018fea2ded1d62f7c7edccadf478ff0906bc27bc
yuliya <yulya.s.n@bk.ru>
2022-11-18 11:01:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2500;
+        int bonusMiles = 20;
+
+        int bonus = price / bonusMiles;
+
+        System.out.println("Yulya, hello!Your bonus" + bonus);
+    }
+}
